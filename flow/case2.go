826d3fd66f5eeb2c456658a1338110b2167a4f56
Scott Stout <j.scott.stout@gmail.com>
2018-04-14 16:15:07 -0700

    2 courses

---
+++ flow/case2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	switch "Jenny" {
+	case "Time", "Jenny":
+		fmt.Println("J & T got here")
+	case "Mahdi":
+		fmt.Println("Mdog")
+	}
+
+}
---
+++ flow/ifstate.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	b := true
+	if food := "something"; b {
+		fmt.Println(food)
+	}
+}
